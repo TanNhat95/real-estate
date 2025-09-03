54cccc0e2445074cd80ff59675ab79ca22f9a4f5
Tan Nhat <nguyentannhat246@gmail.com>
2025-09-03 10:03:38 +0700

    feat(be): get current residences

--- server/src/routes/tenantRoutes.ts
+++ server/src/routes/tenantRoutes.ts
@@ -4,12 +4,14 @@ import {
   createTenant,
   getTenant,
   updateTenant,
+  getCurrentResidences,
 } from "../controllers/tenantControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getTenant);
 router.put("/:cognitoId", updateTenant);
+router.get("/:cognitoId/current-residences", getCurrentResidences);
 router.post("/", createTenant);
 
 export default router;
