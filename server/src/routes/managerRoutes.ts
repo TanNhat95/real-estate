fc353c96a6d0e7d1dd34aba36574a4a3125ec8a6
Tan Nhat <nguyentannhat246@gmail.com>
2025-09-03 10:01:25 +0700

    feat(be): get manager properties

--- server/src/routes/managerRoutes.ts
+++ server/src/routes/managerRoutes.ts
@@ -4,12 +4,14 @@ import {
   createManager,
   getManager,
   updateManager,
+  getManagerProperties,
 } from "../controllers/managerControllers";
 
 const router = express.Router();
 
 router.get("/:cognitoId", getManager);
 router.put("/:cognitoId", updateManager);
+router.get("/:cognitoId/properties", getManagerProperties);
 router.post("/", createManager);
 
 export default router;
