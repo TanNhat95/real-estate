40cd47aa36a0b8625ad1eebbb15e779c7fc4b4cd
Tan Nhat <nguyentannhat246@gmail.com>
2025-09-07 15:19:31 +0700

    feat: applications for manager

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -263,6 +263,48 @@ export const api = createApi({
       },
     }),
 
+    // application related endpoints
+    getApplications: build.query<
+      Application[],
+      { userId?: string; userType?: string }
+    >({
+      query: (params) => {
+        const queryParams = new URLSearchParams();
+        if (params.userId) {
+          queryParams.append("userId", params.userId.toString());
+        }
+        if (params.userType) {
+          queryParams.append("userType", params.userType);
+        }
+
+        return `applications?${queryParams.toString()}`;
+      },
+      providesTags: ["Applications"],
+      async onQueryStarted(_, { queryFulfilled }) {
+        await withToast(queryFulfilled, {
+          error: "Failed to fetch applications.",
+        });
+      },
+    }),
+
+    updateApplicationStatus: build.mutation<
+      Application & { lease?: Lease },
+      { id: number; status: string }
+    >({
+      query: ({ id, status }) => ({
+        url: `applications/${id}/status`,
+        method: "PUT",
+        body: { status },
+      }),
+      invalidatesTags: ["Applications", "Leases"],
+      async onQueryStarted(_, { queryFulfilled }) {
+        await withToast(queryFulfilled, {
+          success: "Application status updated successfully!",
+          error: "Failed to update application settings.",
+        });
+      },
+    }),
+
     createApplication: build.mutation<Application, Partial<Application>>({
       query: (body) => ({
         url: `applications`,
@@ -323,6 +365,8 @@ export const {
   useGetPropertyQuery,
   useAddFavoritePropertyMutation,
   useRemoveFavoritePropertyMutation,
+  useGetApplicationsQuery,
+  useUpdateApplicationStatusMutation,
   useCreateApplicationMutation,
   useGetLeasesQuery,
   useGetPropertyLeasesQuery,
